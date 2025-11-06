c7d363b1ecd4d85d19e0e7fedb3405f552a4dc79
Will Collins <williamacollins05@gmail.com>
2025-11-06 13:50:48 -0600

    Deleted useless files

---
+++ components/festival-viewer/scene-viewer.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import {
+  AvailableItems,
+  CanvasItems,
+  ItemGroup,
+} from "@/types/festival-viewer-types";
+import {
+  createNewFestival,
+  updateFestivalPlacedProps,
+  getFestivalById
+} from "@/lib/supabase/queries";
+import { useSession } from "next-auth/react";
+
+import SceneViewOnly from './scene-view-only';
+import AccountSidebarView from './account-sidebar-view';
+
+export default function SceneBuilder(props: {
+  itemGroups: ItemGroup[];
+  isNew: boolean;
+  id: number;
+}) {
+  const { data: session, status } = useSession();
+
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [canvasItems, setCanvasItems] = useState<CanvasItems[]>([]);
+
+  if (!isLoggedIn && session?.user.id !== null && status === "authenticated") {
+    setIsLoggedIn(true);
+  }
+ 
+  useEffect(() => {
+    const loadFestivalById = async () => {
+      const festivalData = await getFestivalById(props.id)
+      setCanvasItems(festivalData.placed_props_json);
+    }
+
+    if (props.id !== -1 && props.id !== null) {
+      loadFestivalById()
+    }
+    
+  }, [])
+
+  if (!isLoggedIn) {
+    return <h1>You must log in to continue</h1>;
+  }
+
+  return (
+    <div className="flex h-screen bg-gray-100">
+      {/* Sidebar */}
+      <AccountSidebarView />
+
+      {/* Main Canvas Area */}
+      <div className="flex-1 flex flex-col">
+        {/* Canvas */}
+        <SceneViewOnly canvasItems={canvasItems}/>
+      </div>
+    </div>
+  );
+}
